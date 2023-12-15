c31e67df79d58f6ca8640581f451281794f58bfe
yanniksingh <>
2023-12-14 18:44:57 -0800

    refactor decompress.c

--- decompress.c
+++ decompress.c
@@ -1,20 +1,48 @@
 #include "common.h"
 
-typedef struct reverse_code {
+typedef struct reverse_code_table_entry {
 	code_t parent;
-	unsigned char symbol;
-} reverse_code;
+	unsigned char byte;
+} reverse_code_table_entry;
 
-static inline size_t reset_reverse_code_table(reverse_code* reverse_codes, size_t reverse_codes_size) {
-	memset(reverse_codes, 0, reverse_codes_size * sizeof(reverse_code));
+typedef struct reverse_code_table {
+	reverse_code_table_entry* entries;
+	size_t next_available_code;
+	size_t size;
+} reverse_code_table;
 
-	size_t next_available_code = FIRST_AVAILABLE_CODE;
+static inline void reverse_code_table_get(reverse_code_table* table, code_t code, code_t* parent, unsigned char* byte) {
+	*byte = table->entries[code].byte;
+	*parent = table->entries[code].parent;
+}
+
+static inline void reverse_code_table_add(reverse_code_table* table, code_t parent, unsigned char byte) {
+	table->entries[table->next_available_code].parent = parent;
+	table->entries[table->next_available_code].byte = byte;
+	table->next_available_code++;
+}
+
+static inline bool reverse_code_table_out_of_range(reverse_code_table* table, code_t code) {
+	return code >= table->next_available_code;
+}
+
+static inline void reverse_code_table_reset(reverse_code_table* table) {
+	memset(table->entries, 0, table->size * sizeof(reverse_code_table_entry));
+
+	table->next_available_code = FIRST_AVAILABLE_CODE;
 	for (size_t byte = 0; byte <= UCHAR_MAX; byte++) {
-		reverse_codes[next_available_code].parent = NULL_CODE;
-		reverse_codes[next_available_code].symbol = (unsigned char)byte;
-		next_available_code++;
+		reverse_code_table_add(table, NULL_CODE, byte);
 	}
-	return next_available_code;
+}
+
+static inline void reverse_code_table_init(reverse_code_table* table, size_t size) {
+	table->size = size;
+	table->entries = malloc(size * sizeof(reverse_code_table_entry));
+	reverse_code_table_reset(table);
+}
+
+static inline void reverse_code_table_free(reverse_code_table* table) {
+	free(table->entries);
 }
 
 typedef struct buffer {
@@ -40,9 +68,6 @@ static inline size_t buffer_read(buffer* b, code_t* item, FILE* input_file) {
 }
 
 int main(int argc, char** argv) {
-	reverse_code* reverse_codes = malloc(MAX_NUM_CODES * sizeof(reverse_code));
-	size_t next_available_code = reset_reverse_code_table(reverse_codes, MAX_NUM_CODES);
-
 	const char* input_path = argv[1];
 	const char* output_path = argv[2];
 
@@ -50,23 +75,23 @@ int main(int argc, char** argv) {
 	FILE* output_file = fopen(output_path, "wb");
 	flockfile(output_file);
 
-	code_t code;
-	code_t prev_code = NULL_CODE;
-	unsigned char* symbols = malloc(MAX_NUM_CODES * sizeof(unsigned char));
-	int symbols_top = 0;
+	reverse_code_table table;
+	reverse_code_table_init(&table, MAX_NUM_CODES);
 	buffer b;
 	buffer_init(&b);
+	unsigned char* symbols = malloc(MAX_NUM_CODES * sizeof(unsigned char));
+	int symbols_top = 0;
+	code_t code;
+	code_t prev_code = NULL_CODE;
 	while(buffer_read(&b, &code, input_file)) {
 		if (code == NULL_CODE) {
-			next_available_code = reset_reverse_code_table(reverse_codes, MAX_NUM_CODES);
+			reverse_code_table_reset(&table);
 			prev_code = NULL_CODE;
 			symbols_top = 0;
 		}
-		else if (code >= next_available_code) {
+		else if (reverse_code_table_out_of_range(&table, code)) {
 			unsigned char first_prev_symbol = symbols[symbols_top - 1];
-			reverse_codes[next_available_code].parent = prev_code;
-			reverse_codes[next_available_code].symbol = first_prev_symbol;
-			next_available_code++;
+			reverse_code_table_add(&table, prev_code, first_prev_symbol);
 
 			memmove(symbols + 1, symbols, symbols_top * sizeof(unsigned char));
 			symbols[0] = first_prev_symbol;
@@ -76,14 +101,13 @@ int main(int argc, char** argv) {
 			code_t cur_code = code;
 			symbols_top = 0;
 			while (cur_code != NULL_CODE) {
-				symbols[symbols_top] = reverse_codes[cur_code].symbol;
+				unsigned char byte;
+				reverse_code_table_get(&table, cur_code, &cur_code, &byte);
+				symbols[symbols_top] = byte;
 				symbols_top++;
-				cur_code = reverse_codes[cur_code].parent;
 			}
 			if (prev_code != NULL_CODE) {
-				reverse_codes[next_available_code].parent = prev_code;
-				reverse_codes[next_available_code].symbol = symbols[symbols_top - 1];
-				next_available_code++;
+				reverse_code_table_add(&table, prev_code, symbols[symbols_top - 1]);
 			}
 		}
 		prev_code = code;
@@ -91,6 +115,7 @@ int main(int argc, char** argv) {
 			putc_unlocked(symbols[pos], output_file);
 		}
 	}
+	reverse_code_table_free(&table);
 
 	funlockfile(output_file);
 	fclose(output_file);
